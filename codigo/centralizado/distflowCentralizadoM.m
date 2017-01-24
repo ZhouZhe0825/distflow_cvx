063c3a13ff0e7901f0e13384e4bfcb7fa5c0724e
Unknown <salaggia@uni.ort.edu.uy>
2017-01-24 19:47:16 -0300

    Reduccion de variables en M y reordenamiento codigo
    Se redujo la cantidad de variables empleadas en las cargas no interrumpibles para el centralizado M. Ademas se reordeno el codigo de ambos centralizados para que se parezca al master branch

--- codigo/centralizado/distflowCentralizadoM.m
+++ codigo/centralizado/distflowCentralizadoM.m
@@ -1,20 +1,16 @@
 function [Var, opt] = distflowCentralizadoM(Data, Config)
-% function [Var, opt] = distflow_reduced(Data)
 
-
+%% Inicializacion
 VertI = VertIMat(Data.Red.Branch.T);
 VertJ = VertJMat(Data.Red.Branch.T);
-OutBr = OutBrMat(Data.Red.Branch.T);
-InBr = InBrMat(Data.Red.Branch.T);
+OutBr = VertI';
+InBr = VertJ';
 
 n = size(VertI,2);
 m = size(VertI,1);
 
-Data.Red.Branch.rm = NxNxT2MxT(VertI,VertJ,Data.Red.Branch.r);
-Data.Red.Branch.xm = NxNxT2MxT(VertI,VertJ,Data.Red.Branch.x);
-Data.Red.Branch.lTopm = NxNxT2MxT(VertI,VertJ,Data.Red.Branch.lTop);
-Data.Red.Branch.yTopm = NxNxT2MxT(VertI,VertJ,Data.Red.Branch.yTop);
-Data.Red.Branch.yLowm = NxNxT2MxT(VertI,VertJ,Data.Red.Branch.yLow);
+G = find(Data.Gen.Tras.I == 1);
+NnoG = setdiff((1:n),G);
 
 Data.Red.Branch.Tup = triu(Data.Red.Branch.T);
 [rowTup, colTup, ~] = find(Data.Red.Branch.Tup == 1);
@@ -27,8 +23,6 @@ for i=1:length(Tupmind)
     Tupm(i) = find(Tind == Tupmind(i));
     Tdownm(i) = find(Tind == Tdownmind(i));
 end
-G = find(Data.Gen.Tras.I == 1);
-NnoG = setdiff((1:n),G);
 
 tnnLow = (1 + Data.Red.Bus.TapLow.*Data.Red.Bus.Ntr);
 tnnTop = (1 + Data.Red.Bus.TapTop.*Data.Red.Bus.Ntr);
@@ -44,8 +38,7 @@ NcpCapTvT = Data.Red.Bus.Ncp.*Data.Red.Bus.CapTop.*(Data.Red.Bus.uTop).^2;
 NcpCapLvT = Data.Red.Bus.Ncp.*Data.Red.Bus.CapLow.*(Data.Red.Bus.uTop).^2;
 NcpCapTvL = Data.Red.Bus.Ncp.*Data.Red.Bus.CapTop.*(Data.Red.Bus.uLow).^2;
 
-
-
+% Cargas No interrumpibles
 ClNI = find(Data.ClNI.I == 1);
 nClNI = length(ClNI);
 
@@ -56,10 +49,8 @@ qTClNI = Data.Util.qzCnTopE(ClNI,:);
 vLClNI = Data.Red.Bus.uLow(ClNI,:).^2;
 vTClNI = Data.Red.Bus.uTop(ClNI,:).^2;
 
-uLowApp = zeros(size(Data.Red.Bus.uLow,1),size(Data.Red.Bus.uLow,2), 2);
-uTopApp = uLowApp;
 
-iC = Data.Red.Bus.indCons;
+%% Modelo programacion matematica
 
 tic
 cvx_begin
@@ -70,53 +61,57 @@ cvx_begin
 
     cvx_precision high
     %% Declaracion de variables y expresiones
-
+    %Variables generales de la red
 	variable P(m, Config.Etapas); % Potencia activa en el arco i,j
 	variable Q(m, Config.Etapas); % Potencia reactiva en el arco i,j
 	variable l(m, Config.Etapas); % Corriente en el arco i,j
-	variable pN(n, Config.Etapas); % Consumo de potencia activa en el nodo i
-	variable qN(n, Config.Etapas); % Consumo de potencia reactiva en el nodo i
+    variable z(m, Config.Etapas);
+    variable y(m, Config.Etapas) binary;
+    variable w(n, Config.Etapas);
+    	
 	variable v(n, Config.Etapas); % Modulo^2 de la tension
 	variable cDv(n, Config.Etapas); % Modulo^2 de la tension
-    variable nn(n, Config.Etapas); % module of square complex voltage in i
-    variable nv(n, Config.Etapas); % module of square complex voltage in i
-    variable w(n, Config.Etapas);
+    variable nn(n, Config.Etapas);
+    variable nv(n, Config.Etapas);
     variable Tap(n, Config.Etapas) integer;
-    variable Cap(n, Config.Etapas) integer;
+
+    variable pC(n, Config.Etapas); % Consumo de potencia activa en el nodo i
+	variable qC(n, Config.Etapas); % Consumo de potencia reactiva en el nodo i
+
+    variable pN(n, Config.Etapas); % Consumo de potencia activa en el nodo i
+	variable qN(n, Config.Etapas); % Consumo de potencia reactiva en el nodo i
 
     variable pG(n, Config.Etapas); % Potencia activa generada en el nodo i
 	variable qG(n, Config.Etapas); % Potencia reactiva generada en el nodo i
+
     variable qCp(n, Config.Etapas); % reactive power demand in i
-    variable cQG(n, Config.Etapas);
+    variable Cap(n, Config.Etapas) integer;
     
-	variable pC(n, Config.Etapas); % Consumo de potencia activa en el nodo i
-	variable qC(n, Config.Etapas); % Consumo de potencia reactiva en el nodo i
+    variable pGTras(n, Config.Etapas);
+    variable qGTras(n, Config.Etapas);
+    variable cqGTras(n, Config.Etapas);
     
-    variable z(m, Config.Etapas);
-    variable y(m, Config.Etapas) binary;
-    
-    
- 	expression lQoL(m, Config.Etapas, 3);
- 	expression lNorm(m, Config.Etapas, 3);
-    expression vExpr(n, Config.Etapas);
-    expression CapDif(n,Config.Etapas);
-    expression TapDif(n,Config.Etapas);
-
-    variable pCClNI(n, Config.Etapas);
-    variable qCClNI(n, Config.Etapas);
-
     variable pCApp(n, Config.Etapas, 2); % real power demand in i
     variable qCApp(n, Config.Etapas, 2); % real power demand in i
-    expression vApp(n, Config.Etapas, 2);
     variable pCn(n, Config.Etapas, 2); % real power demand in i
-    
-    
     variable pCClRes(n, Config.Etapas); % real power demand in i
     variable qCClRes(n, Config.Etapas); % real power demand in i
     
-	expression tfopt_expr(Config.Etapas,1); 
-	expression fopt_expr; 
+    variable pCClNI(n, Config.Etapas);
+    variable qCClNI(n, Config.Etapas);
+	
+ 	expression lQoL(m, Config.Etapas, 3);
+ 	expression lNorm(m, Config.Etapas, 3);
+    expression vExpr(n, Config.Etapas);
+    expression vApp(n, Config.Etapas, 2);
+    expression CapDif(n,Config.Etapas);
+    expression TapDif(n,Config.Etapas);
 
+    expression tfopt_expr(Config.Etapas,1); 
+	expression fopt_expr; 
+    
+    
+    %% Funcion objetivo
     CapDif(:,1) = Data.Red.Bus.CapIni;
     CapDif(:,(2:Config.Etapas)) = Cap(:,(2:Config.Etapas)) - Cap(:,(1:Config.Etapas-1));
 
@@ -124,31 +119,31 @@ cvx_begin
     TapDif(:,(2:Config.Etapas)) = Tap(:,(2:Config.Etapas)) - Tap(:,(1:Config.Etapas-1));
         
 	tfopt_expr = ...
-        sum(Data.Cost.piPTrasm.*pG,1) ...
-        + sum(Data.Cost.cdvm.*cDv,1) ...
-        + sum(cQG,1) ...
+        sum(Data.Cost.piPTras.*pGTras,1) ...
+        + sum(Data.Cost.cdv.*cDv,1) ...
+        + sum(cqGTras,1) ...
         + sum(Data.Red.cambioCap*Data.Red.Bus.indCap.*(CapDif.^2),1) ...
         + sum(Data.Red.cambioTap*Data.Red.Bus.indTap.*(TapDif.^2),1) ...
         + sum(Data.Util.betaT(:,:,1).*((pCn(:,:,1) - Data.Util.pzCnPref(:,:,1)).^2),1) ...
         ;
 
-% 	fopt_expr = sum(Data.Cost.piPTrasm.*pg) + sum(Data.Cost.cdvm.*cDv);
+    cqGTras >= - Data.Cost.piQmtras .* qGTras;
+    cqGTras >= Data.Cost.piQMtras .* qGTras;
 
 
-    cQG >= - Data.Cost.piQmtras .* qG;
-    cQG >= Data.Cost.piQMtras .* qG;
-
-
-	% Restriccion de balance de potencia consumida y generada
-    pN == InBr*(P - Data.Red.Branch.rm.*l) - OutBr*P;
-    qN == InBr*(Q - Data.Red.Branch.xm.*l) - OutBr*Q;
+    %% Restricciones de Red
+    % Restricciones de potencias por nodo
+    pN == InBr*(P - Data.Red.Branch.r.*l) - OutBr*P;
+    qN == InBr*(Q - Data.Red.Branch.x.*l) - OutBr*Q;
 
     pC == pCClRes + pCClNI;
     qC == qCClRes + qCClNI;
-    
+
+    pG == pGTras;
+    qG == qGTras + qCp;
 
     pN - pC + pG == 0;
-    qN - qC + qG + qCp == 0;
+    qN - qC + qG == 0;
     
     % Restricciones de capacitores
     Cap >= Data.Red.Bus.CapLow;
@@ -159,12 +154,10 @@ cvx_begin
     qCp <= NcpCapL.*v + NcpvT.*Cap - NcpCapLvT;
     qCp <= NcpCapT.*v + NcpvL.*Cap - NcpCapTvL;
     
-
-	% Restriccion conica de corriente
+	% Restricciones conica de corriente
 	lQoL(:,:,1) = 2 * P;
 	lQoL(:,:,2) = 2 * Q;
 	lQoL(:,:,3) = l - VertI*v;
-	
 	norms(lQoL,2,3) <= l + VertI*v;
 
     lNorm(:,:,1) = 2*P;
@@ -172,7 +165,6 @@ cvx_begin
     lNorm(:,:,3) =  l - (VertI*Data.Red.Bus.uTop.^2).*z;
     norms(lNorm,2,3) <= l + (VertI*Data.Red.Bus.uTop.^2).*z;
     
-	
 	% Restriccion de la tension
     nn >= (1 + Tap.*Data.Red.Bus.Ntr).^2;
     nn <= (tnnTop + tnnLow).*(1 + Tap.*Data.Red.Bus.Ntr) - (tnnTop.*tnnLow);
@@ -186,7 +178,7 @@ cvx_begin
     Tap >= Data.Red.Bus.TapLow;
     Tap <= Data.Red.Bus.TapTop;
     
-    vExpr = VertI*nv - VertJ*v - 2 * (Data.Red.Branch.rm.*P + Data.Red.Branch.xm.*Q) + ((Data.Red.Branch.rm).^2 + (Data.Red.Branch.xm).^2) .* l;
+    vExpr = VertI*nv - VertJ*v - 2 * (Data.Red.Branch.r.*P + Data.Red.Branch.x.*Q) + ((Data.Red.Branch.r).^2 + (Data.Red.Branch.x).^2) .* l;
     
     0 >= vExpr - (VertI*(Data.Red.Bus.uTop.^2 - Data.Red.Bus.uLow.^2)).*(1-z);
     0 <= vExpr + (VertI*(Data.Red.Bus.uTop.^2 - Data.Red.Bus.uLow.^2)).*(1-z);
@@ -195,7 +187,6 @@ cvx_begin
     cDv >= (v - (1+Data.Cost.delta))*diag(Data.Cost.m);
     cDv >= (v - (1-Data.Cost.delta))*diag(-Data.Cost.m);
 
-    
     % Restricciones de arcos de la Red
     z(Tupm,:) + z(Tdownm,:) == y(Tupm,:);
     InBr(NnoG,:)*z == 1; % los nodos de que no son barras estan todos conectados
@@ -207,24 +198,26 @@ cvx_begin
 	w >= 0;
 	w <= n-1;
 	
+    %% Restricciones de generacion
+	% Restricciones de Trasmision
+    pGTras >= Data.Gen.Tras.pgLow;
+    pGTras <= Data.Gen.Tras.pgTop;
+    qGTras >= Data.Gen.Tras.qgLow;
+    qGTras <= Data.Gen.Tras.qgTop;
     
-    
-	% Restricciones de generacion
-    pG >= Data.Gen.Tras.pgLowm;
-    pG <= Data.Gen.Tras.pgTopm;
-    qG >= Data.Gen.Tras.qgLowm;
-    qG <= Data.Gen.Tras.qgTopm;
-	
-	% Restricciones de caja
+	%% Restricciones de caja
 	v >= Data.Red.Bus.uLow.^2;
 	v <= Data.Red.Bus.uTop.^2;
-	l <= Data.Red.Branch.lTopm.*z;
+	l <= Data.Red.Branch.lTop.*z;
     z >= 0;
     z <= 1;
-    y >= Data.Red.Branch.yLowm;
-    y <= Data.Red.Branch.yTopm;
+    y >= Data.Red.Branch.yLow;
+    y <= Data.Red.Branch.yTop;
 
-    %% Cliente Residencial
+    %% Restricciones de clientes residenciales
+    uLowApp = zeros(size(Data.Red.Bus.uLow,1),size(Data.Red.Bus.uLow,2), 2);
+    uTopApp = uLowApp;
+
     for app = 1:2
         vApp(:,:,app) = v(:,:);
         uLowApp(:,:,app) = Data.Red.Bus.uLow(:,:);
@@ -277,66 +270,62 @@ cvx_begin
 
     fopt_expr = sum(tfopt_expr);
     minimize fopt_expr
-
     
 cvx_end
-cvx_status
 toc
 
-%% Reconstruccion de la solucion
+%% Construccion de la estructura de solucion
 % pasaje a NxNxT
+Var.Red.Branch.P = MxT2NxNxT(VertI,VertJ,P);
+Var.Red.Branch.Q = MxT2NxNxT(VertI,VertJ,Q);
+Var.Red.Branch.l = MxT2NxNxT(VertI,VertJ,l);
+Var.Red.Branch.z = MxT2NxNxT(VertI,VertJ,z);
+Var.Red.Branch.y = MxT2NxNxT(VertI,VertJ,y);
+Var.Red.Bus.w = permute(full(w), [1 3 2]);
 
-	Var.Red.Branch.P = MxT2NxNxT(VertI,VertJ,P);
-	Var.Red.Branch.Q = MxT2NxNxT(VertI,VertJ,Q);
-	Var.Red.Branch.l = MxT2NxNxT(VertI,VertJ,l);
-	Var.Red.Bus.v = permute(full(v), [1 3 2]);
-	Var.Red.Bus.cDv = permute(full(cDv), [1 3 2]);
-	Var.Red.Bus.nv = permute(full(nv), [1 3 2]);
-	Var.Red.Bus.nn = permute(full(nn), [1 3 2]);
-	Var.Red.Bus.Tap = permute(full(Tap), [1 3 2]);
-	Var.Red.Bus.Cap = permute(full(Cap), [1 3 2]);
+Var.Red.Bus.v = permute(full(v), [1 3 2]);
+Var.Red.Bus.cDv = permute(full(cDv), [1 3 2]);
+Var.Red.Bus.nn = permute(full(nn), [1 3 2]);
+Var.Red.Bus.nv = permute(full(nv), [1 3 2]);
+Var.Red.Bus.Tap = permute(full(Tap), [1 3 2]);
 
-	Var.Red.Bus.pC = permute(full(pC), [1 3 2]);
-	Var.Red.Bus.qC = permute(full(qC), [1 3 2]);
+Var.Red.Bus.pC = permute(full(pC), [1 3 2]);
+Var.Red.Bus.qC = permute(full(qC), [1 3 2]);
 
-	Var.Red.Bus.pN = permute(full(pN), [1 3 2]);
-	Var.Red.Bus.qN = permute(full(qN), [1 3 2]);
+Var.Red.Bus.pN = permute(full(pN), [1 3 2]);
+Var.Red.Bus.qN = permute(full(qN), [1 3 2]);
 
-	Var.Red.Bus.pG = permute(full(pG), [1 3 2]);
-	Var.Red.Bus.qG = permute(full(qG), [1 3 2]);
-	Var.Red.Bus.qCp = permute(full(qCp), [1 3 2]);
+Var.Red.Bus.pG = permute(full(pG), [1 3 2]);
+Var.Red.Bus.qG = permute(full(qG), [1 3 2]);
 
-	Var.Red.Branch.y = MxT2NxNxT(VertI,VertJ,y);
-	Var.Red.Branch.z = MxT2NxNxT(VertI,VertJ,z);
-	Var.Red.Bus.w = permute(full(w), [1 3 2]);
+Var.Red.Bus.qCp = permute(full(qCp), [1 3 2]);
+Var.Red.Bus.Cap = permute(full(Cap), [1 3 2]);
 
-    Var.ClRes.pC = permute(full(pCClRes), [1 3 2]);
-    Var.ClRes.qC = permute(full(qCClRes), [1 3 2]);
+Var.Red.Bus.PTras = zeros(n,1,Config.Etapas);
+Var.Red.Bus.PTras(G,1,:) = pGTras(G,:);
+Var.Red.Bus.QTras = zeros(n,1,Config.Etapas);
+Var.Red.Bus.QTras(G,1,:) = qGTras(G,:);
 
-    Var.ClRes.pCApp = permute(full(pCApp), [1 4 2 3]);
-    Var.ClRes.qCApp = permute(full(qCApp), [1 4 2 3]);
+Var.ClRes.pCApp = permute(full(pCApp), [1 4 2 3]);
+Var.ClRes.qCApp = permute(full(qCApp), [1 4 2 3]);
+Var.ClRes.pC = permute(full(pCClRes), [1 3 2]);
+Var.ClRes.qC = permute(full(qCClRes), [1 3 2]);
 
-    Var.Red.Bus.PTras = zeros(n,1,Config.Etapas);
-    Var.Red.Bus.PTras(G,1,:) = sum(Var.Red.Branch.P(G,:,:),2);
-    Var.Red.Bus.QTras = zeros(n,1,Config.Etapas);
-    Var.Red.Bus.QTras(G,1,:) = sum(Var.Red.Branch.Q(G,:,:),2);
+if nClNI > 0
+	Var.ClNI.pC = pCClNI;
+	Var.ClNI.qC = qCClNI;
+	Var.ClNI.on = pCClNI*0;
+	Var.ClNI.on(ClNI,:) = onClNI;
+	Var.ClNI.start = pCClNI*0;
+	Var.ClNI.start(ClNI,:) = stClNI;
+	
+	Var.ClNI.pC = permute(full(Var.ClNI.pC), [1 3 2]);
+	Var.ClNI.qC = permute(full(Var.ClNI.qC), [1 3 2]);
+	Var.ClNI.on = permute(full(Var.ClNI.on), [1 3 2]);
+	Var.ClNI.start = permute(full(Var.ClNI.start), [1 3 2]);
+	
+end
 
-    if nClNI > 0
-        Var.ClNI.pC = pCClNI;
-        Var.ClNI.qC = qCClNI;
-        Var.ClNI.on = pCClNI*0;
-        Var.ClNI.on(ClNI,:) = onClNI;
-        Var.ClNI.start = pCClNI*0;
-        Var.ClNI.start(ClNI,:) = stClNI;
-        
-        Var.ClNI.pC = permute(full(Var.ClNI.pC), [1 3 2]);
-        Var.ClNI.qC = permute(full(Var.ClNI.qC), [1 3 2]);
-        Var.ClNI.on = permute(full(Var.ClNI.on), [1 3 2]);
-        Var.ClNI.start = permute(full(Var.ClNI.start), [1 3 2]);
-        
-    end
-    
-    
 opt = fopt_expr;
 cvx_clear
 end
